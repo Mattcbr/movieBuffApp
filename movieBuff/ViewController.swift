860942547706637c57ef25beed0dedb1e991b416
mattcbr <matheus.castelo@gmail.com>
2019-01-23 15:16:10 -0200

    Creating Project and Adding Icon

---
+++ movieBuff/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  movieBuff
+//
+//  Created by Matheus Queiroz on 1/23/19.
+//  Copyright © 2019 mattcbr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
